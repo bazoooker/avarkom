c0b536785410b04fc774e2c8a4215eb8db8a84e7
Alex <manakov55@yandex.ru>
2018-03-05 19:31:24 +0700

    css done

--- distrib/js/script.js
+++ distrib/js/script.js
@@ -1,8 +1,22 @@
-var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 2,
-      spaceBetween: 20,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
+
+
+	var swiper = new Swiper('.swiper-container', {
+	  slidesPerView: 2,
+	  spaceBetween: 20,
+	  navigation: {
+	    nextEl: '.swiper-button-next',
+	    prevEl: '.swiper-button-prev',
+	  },
+	  breakpoints: {	    
+	    768: {
+	      slidesPerView: 1,
+	      spaceBetween: 10
+	    }
+	  }
+	});
+
+	$('.form-action').mask('+7(000)000-00-00');
+
+	// $(".hero_top").paroller({ factor: '0.5', type: 'foreground', direction: 'vertical' });
+
+
